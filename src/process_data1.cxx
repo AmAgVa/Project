46eea4f1da229370a86c0c0a04b32e83dcaad5ee
Nathaly3162 <143444228+Nathaly3162@users.noreply.github.com>
2023-10-27 02:08:34 +0200

    Update process_data1.cxx

--- src/process_data1.cxx
+++ src/process_data1.cxx
@@ -7,7 +7,7 @@
 #include "temperature_data.h"
 
 int processTemperatureData1() {
-    std::ifstream input("cleansed.csv");
+    std::ifstream input("cleaneddata_Lund.csv");
 
     std::vector<TemperatureData> data;
     std::map<int, TemperatureData> warmestDays;
